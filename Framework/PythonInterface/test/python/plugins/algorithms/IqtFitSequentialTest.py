a9e3e2dd9a5e7e39d706d58d990f3d1af250783f
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-02-24 15:11:37 +0000

    Add files for algorithm
    Refs #15343

---
+++ Framework/PythonInterface/test/python/plugins/algorithms/IqtFitSequentialTest.py
@@ -0,0 +1,23 @@
+import unittest
+from mantid.simpleapi import *
+from mantid.api import MatrixWorkspace, WorkspaceGroup, ITableWorkspace
+
+
+class IqtFitSequentialTest(unittest.TestCase):
+
+#------------------------------------------Set up------------------------------------------
+
+
+#-----------------------------------Validation of result-----------------------------------
+
+
+
+#--------------------------------------Success cases---------------------------------------
+
+
+
+#--------------------------------------Failure cases---------------------------------------
+
+
+if __name__=="__main__":
+    unittest.main()
